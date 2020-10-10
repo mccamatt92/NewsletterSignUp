d022a39f8b8ce8a32248312f08574485d5ca785e
mccamatt92 <matthias.mccarthy@gmail.com>
2020-10-10 18:11:05 +0200

    Changed Folders

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 const port = 3000;
+
+
+app.get("/",function(req,res){
+    res.sendFile(`${__dirname}/signup.html`)
+})
+
+app.post("/",function(req,res){
+    var firstname = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+    
+})
 
 
 app.listen(port,function(){
     console.log(`Server Is Running on ${port}`)
 });
+
+
