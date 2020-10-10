a98b9931e436047e2361e351e8b8151fde7528a4
mccamatt92 <matthias.mccarthy@gmail.com>
2020-10-10 17:57:24 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port,function(){
+    console.log(`Server Is Running on ${port}`)
+});
