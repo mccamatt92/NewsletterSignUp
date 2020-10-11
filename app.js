895662eb23ed8fb22637b7d77ee9ff8e5fddee22
mccamatt92 <matthias.mccarthy@gmail.com>
2020-10-11 20:03:47 +0200

    Added api call

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https")
 const request = require("request");
 
+
+const api = {
+    key: "Insertkeyhere",
+    url: `https://us2.api.mailchimp.com/3.0/lists/bddfe0d70e`
+}
+
+
 const app = express();
+
+
+const options ={
+    method:"POST",
+    auth:`matthias:${api.key}`
+
+}
+
+
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 const port = 3000;
@@ -13,11 +31,38 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    var firstname = req.body.fname;
-    var lastName = req.body.lname;
-    var email = req.body.email;
-    
+    const firstname = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    var data = {
+        members: [
+            {
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:firstname,
+                LNAME:lastName
+            }
+        }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+
+  const request =  https.request(api.url,options,function(respone)
+    {
+        respone.on("data",function(data)
+        {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+request.end();
 })
+
+
 
 
 app.listen(port,function(){
